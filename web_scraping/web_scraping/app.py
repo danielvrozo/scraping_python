e531a752eded0ecfd63f5e207360770f2fa6b481
danielvrozo <danielvegarozo99@gmail.com>
2021-01-21 11:56:21 -0500

    Update app.py

--- web_scraping/web_scraping/app.py
+++ web_scraping/web_scraping/app.py
@@ -1,18 +1,18 @@
-import bancopopular
-from flask import Flask, render_template
+from bancopopular import scraping
+from flask import Flask
 import os
 import sys
 
-
 app = Flask(__name__)
-
-@app.route('/bancopopular')
-def index():
-    return bancopopular.app()
+if __name__ == '__main__': app.run()
     
+@app.route("/bancopopular")
+def popular():
+    scraping_web = scraping()
+    return scraping_web
+    
+
 @app.route('/bancolombia')
 def bancolombia():
     return 'bancolombia'
-    
-if __name__ == '__main__': app.run(debug=True)
 
